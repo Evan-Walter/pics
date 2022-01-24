5d26119613831c01d60c68202bca9e3afa5babf3
Evan Walter <ewpw5678@gmail.com>
2022-01-23 20:34:38 -0800

    starting rebuild

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,8 @@
-import React from 'react';
-import unsplash from '../api/unsplash';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+import React, { Component } from 'react';
+import '../index.css';
 
-class App extends React.Component {
-  state = { images: [] };
-
-  onSearchSubmit = async term => {
-    const response = await unsplash.get(
-      'https://api.unsplash.com/search/photos',
-      {
-        params: { query: term },
-      }
-    );
-
-    this.setState({ images: response.data.results });
-  };
-
+export default class App extends Component {
   render() {
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
-      </div>
-    );
+    return <div>App</div>;
   }
 }
-
-export default App;
