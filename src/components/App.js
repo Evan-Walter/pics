d861007e09c8c84b12db848c9aa1eb54f3c42860
Evan Walter <57516308+Evan-Walter@users.noreply.github.com>
2022-01-27 14:19:06 -0800

    request unsplash api with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import '../index.css';
 import SearchBar from './SearchBar';
 
 export default class App extends React.Component {
   handleSearch(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID FkefCcB6TpHI654RjCohiAAQmoyI3gSxgHMK-w9vLWk',
+      },
+    });
   }
-  
+
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
