2fa49bab07a3dad07b244c0fc85c5d6e8b4745ec
Evan Walter <57516308+Evan-Walter@users.noreply.github.com>
2022-01-27 14:24:51 -0800

    handle res with `.then`

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,17 @@ import SearchBar from './SearchBar';
 
 export default class App extends React.Component {
   handleSearch(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: 'Client-ID FkefCcB6TpHI654RjCohiAAQmoyI3gSxgHMK-w9vLWk',
-      },
-    });
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization:
+            'Client-ID FkefCcB6TpHI654RjCohiAAQmoyI3gSxgHMK-w9vLWk',
+        },
+      })
+      .then((res) => {
+        console.log(res.data.results);
+      });
   }
 
   render() {
